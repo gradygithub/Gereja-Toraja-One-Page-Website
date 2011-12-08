94f0f0accb8ee7bd7f8902b42d19da6b554ffcb8
Gradytama Elkana <odoy_counter_strikes@yahoo.com>
2011-12-09 01:20:56 +0700

    UAS

---
+++ grady/application/views/viewPage.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head> 
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>GRADYTAMA ELKANA | 08120080014</title>
+        <link rel="stylesheet" type="text/css" href="styles.css" />
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+        <script type="text/javascript" src="jquery.scrollTo-1.4.2/jquery.scrollTo-min.js"></script>
+        <script type="text/javascript" src="script.js"></script>
+    </head>
+    
+    <body>
+    	<div class="section" id="page">    
+            <div class="header"> 
+                <h1>GEREJA TORAJA</h1>
+                <h3>by Grady</h3>
+                <div class="nav clear">
+                    <ul>
+                        <li><a href="#article1">About</a></li>
+                        <li><a href="#article2">Location</a></li>
+                        <li><a href="#article3">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="section" id="articles">
+                <div class="line"></div>  
+                <div class="article" id="article1">
+                    <h2>ABOUT</h2>
+                    <div class="line"></div>
+                    <div class="articleBody clear">
+                    	<div class="figure">
+	                    	<img src="img/GEREJA_TORAJA.jpg" width="620" height="340" alt="Me" />
+                        </div>        
+                        <p>Misi Gereja Toraja adalah bersekutu, bersaksi, dan melayani yang dijabarkan dalam berbagai bentuk pelayanan gerejawi.
+						</p>
+						</div>
+                </div>
+                
+                <div class="line"></div>
+                
+                <div class="article" id="article2">
+                    <h2>LOCATION</h2>
+                    <div class="line"></div>
+                    <div class="articleBody clear">
+                    	<div class="figure">
+	                    	<img src="img/map.png" width="620" height="340" alt="College" />
+                        </div>
+                        <p>JL. Jambangan Baru Selatan 8, Surabaya
+						</p>
+						</div>
+                </div>
+           
+                <div class="line"></div>
+				
+                <div class="article" id="article3">
+                    <h2>Contact</h2>
+                    
+                    <div class="line"></div>
+                    
+                    <div class="articleBody clear">
+                    	<div class="figure">
+	                    	<img src="img/contact.jpg" width="620" height="340" alt="Contact" />
+						</div>
+                        <p>FACEBOOK: www.facebook.com/gradytama
+						</p>
+						</div>
+                </div>
+            </div>
+
+        <div class="footer">
+          <div class="line"></div>   
+           <p>Copyright 2011</p>
+           <a href="#" class="up">Go UP</a>
+        </div>
+		
+		</div> 
+    </body>
+</html>
